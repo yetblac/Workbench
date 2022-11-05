b11aa06f6a37d089214c1838cebcd844f0e5ecb7
yetblac <114261768+yetblac@users.noreply.github.com>
2022-11-05 13:12:05 -0400

    Delete jetWork.py

--- jetWork.py
+++
@@ -1,4 +0,0 @@
-testString = "This is a String to Show Operations"
-print(testString[1:6:2])
-    # in this example [1:6] is used to show "This i" then [:2] skips
-    # every other character creating "hsi"
